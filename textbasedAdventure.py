85f98311cc12b8d54d16d6f17457a003b28814a4
Rafiq Rifhan <47004183+RafiqRifhanRosman@users.noreply.github.com>
2019-02-25 15:03:08 +0800

    Update and rename ipad_textbasedAdventure.py to textbasedAdventure.py

--- textbasedAdventure.py
+++ textbasedAdventure.py
@@ -25,6 +25,14 @@ steps = 0
 user_posx = {5}
 user_posy = {0}
 
+found_room1 = 0
+found_room2 = 0
+found_room3 = 0
+found_room4 = 0
+found_room5 = 0
+found_room6 = 0
+total_rooms_found = 0
+
 x = 5
 y = 0
 
@@ -60,10 +68,10 @@ def movement ():
                 x -= steps
                 
         user_posx.clear()
-        user_posx.add(x)
+        user_posx.add(x) #updates the postion of the user in the x axis 
         
         user_posy.clear()
-        user_posy.add(y)
+        user_posy.add(y) #updates the position of the user in the y axis 
         
         print (user_posx)
         print (user_posy)
@@ -99,46 +107,65 @@ def movement ():
        # print (user_posx)
         #print (user_posy
 import time
-timeout = time.time() + 10
+timeout = time.time() + 350
 
 instructions ()
 while True:
         
         
         movement()
-
         if time.time () > timeout:
-                break
-
+            break
         
         if (user_posx.intersection(Hallway_x) != set() and user_posy.intersection(Hallway_y) != set()):
                 print ('You are still in the Hallway')
         
         elif (user_posx.intersection(Room1_x)!= set() and user_posy.intersection(Room1_y)!= set()):
                 print ('You are in Room 1')
+                if (found_room1) == 0:
+                     found_room1 +=1
+                    
                   
         elif (user_posx.intersection(Room2_x) != set() and user_posy.intersection(Room2_y)!= set()):
                 print ('You are in Room 2')
+                if  found_room2 == 0:
+                     found_room2 +=1
                 
         elif (user_posx.intersection(Room3_x)!= set() and user_posy.intersection(Room3_y)!= set()):
                 print ('You are in Room 3')
+                if found_room3 == 0:
+                     found_room3 +=1
                  
         elif (user_posx.intersection(Room4_x)!= set() and user_posy.intersection(Room4_y)!= set()):
                 print ('You are in Room 4')
+                if found_room4 == 0:
+                     found_room4 +=1
                  
         elif (user_posx.intersection(Room5_x)!= set() and user_posy.intersection(Room5_y)!= set()):
                 print ('You are in Room 5')
+                if  found_room5 == 0:
+                     found_room5 +=1
                  
         elif (user_posx.intersection(Room6_x)!= set() and user_posy.intersection(Room6_y)!= set()):
                 print ('You are in Room 6')
+                if  found_room6 == 0:
+                     found_room6 +=1
         
         else:
                 print ('You are walking against the wall! Go back!')
+                
+        
+        total_rooms_found = found_room1 + found_room2 + found_room3 + found_room4 + found_room5 + found_room6
+        if (total_rooms_found) == 6:
+            break
+        print (total_rooms_found)
+                
+                
+        
 
 
 print ('Time\'s up!')
-
-                
+print ("The number of rooms you have found is: " + str(total_rooms_found))
 
                 
 
